f707f01e438a3a7801f99c525cdfc684f928859a
cgray <cgray@yammer-inc.com>
2013-07-22 14:47:11 -0700

    Move to dashboard package.

--- tenacity-dashboard/src/main/java/com/yammer/tenacity/dashboard/TenacityDashboardService.java
+++ tenacity-dashboard/src/main/java/com/yammer/tenacity/dashboard/TenacityDashboardService.java
@@ -1,9 +1,10 @@
-package com.yammer.tenacity.dashboard.bundle;
+package com.yammer.tenacity.dashboard;
 
 import com.yammer.dropwizard.Service;
 import com.yammer.dropwizard.config.Bootstrap;
 import com.yammer.dropwizard.config.Configuration;
 import com.yammer.dropwizard.config.Environment;
+import com.yammer.tenacity.dashboard.bundle.TenacityBundle;
 
 public class TenacityDashboardService extends Service<Configuration> {
     private TenacityDashboardService() {}
