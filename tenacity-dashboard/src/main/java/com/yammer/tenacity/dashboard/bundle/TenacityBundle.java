788ff0f18df963d9c772868d28bd2abd1d774878
cgray <cgray@yammer-inc.com>
2013-07-22 15:59:58 -0700

    ConfiguredBundle to disable gzip.

--- tenacity-dashboard/src/main/java/com/yammer/tenacity/dashboard/bundle/TenacityBundle.java
+++ tenacity-dashboard/src/main/java/com/yammer/tenacity/dashboard/bundle/TenacityBundle.java
@@ -2,20 +2,26 @@ package com.yammer.tenacity.dashboard.bundle;
 
 import com.netflix.hystrix.dashboard.stream.MockStreamServlet;
 import com.netflix.hystrix.dashboard.stream.ProxyStreamServlet;
-import com.yammer.dropwizard.Bundle;
+import com.yammer.dropwizard.ConfiguredBundle;
 import com.yammer.dropwizard.assets.AssetsBundle;
 import com.yammer.dropwizard.config.Bootstrap;
+import com.yammer.dropwizard.config.Configuration;
 import com.yammer.dropwizard.config.Environment;
 import com.yammer.tenacity.dashboard.resources.IndexResource;
 
-public class TenacityBundle implements Bundle {
+public class TenacityBundle implements ConfiguredBundle<Configuration> {
     @Override
     public void initialize(Bootstrap<?> bootstrap) {
         bootstrap.addBundle(new AssetsBundle());
     }
 
     @Override
-    public void run(Environment environment) {
+    public void run(Configuration configuration, Environment environment) throws Exception {
+        configuration
+                .getHttpConfiguration()
+                .getGzipConfiguration()
+                .setEnabled(false);
+
         environment.addServlet(new MockStreamServlet(), "/tenacity/mock.stream");
         environment.addServlet(new ProxyStreamServlet(), "/tenacity/proxy.stream");
 
