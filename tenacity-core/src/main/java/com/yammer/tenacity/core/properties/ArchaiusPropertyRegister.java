41838bd690c8cbf2528b7d26ee914713a5507e8f
cgray <cgray@yammer-inc.com>
2013-08-20 15:35:37 -0700

    Adding the TenacityPollingScehduler to the Archaius register. This allows us to gracefully handle initialLoad failures.

--- tenacity-core/src/main/java/com/yammer/tenacity/core/properties/ArchaiusPropertyRegister.java
+++ tenacity-core/src/main/java/com/yammer/tenacity/core/properties/ArchaiusPropertyRegister.java
@@ -1,14 +1,42 @@
 package com.yammer.tenacity.core.properties;
 
 import com.netflix.config.ConfigurationManager;
+import com.netflix.config.DynamicConfiguration;
+import com.netflix.config.FixedDelayPollingScheduler;
+import com.netflix.config.PolledConfigurationSource;
+import com.netflix.config.sources.URLConfigurationSource;
 import com.yammer.tenacity.core.config.BreakerboxConfiguration;
-import org.apache.commons.configuration.AbstractConfiguration;
+import org.apache.commons.configuration.Configuration;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 
 public class ArchaiusPropertyRegister {
+    private static class TenacityPollingScheduler extends FixedDelayPollingScheduler {
+        private static final Logger LOGGER = LoggerFactory.getLogger(TenacityPollingScheduler.class);
+
+        public TenacityPollingScheduler(int initialDelayMillis, int delayMillis, boolean ignoreDeletesFromSource) {
+            super(initialDelayMillis, delayMillis, ignoreDeletesFromSource);
+        }
+
+        public TenacityPollingScheduler() {
+        }
+
+        @Override
+        protected synchronized void initialLoad(PolledConfigurationSource source, Configuration config) {
+            try {
+                super.initialLoad(source, config);
+            } catch (Exception err) {
+                LOGGER.warn("Initial dynamic configuration load failed", err);
+            }
+        }
+    }
     public static void register(BreakerboxConfiguration breakerboxConfiguration) {
-        final AbstractConfiguration configuration = ConfigurationManager.getConfigInstance();
-        configuration.setProperty("archaius.configurationSource.additionalUrls", breakerboxConfiguration.getUrls());
-        configuration.setProperty("archaius.fixedDelayPollingScheduler.initialDelayMills", breakerboxConfiguration.getInitialDelay());
-        configuration.setProperty("archaius.fixedDelayPollingScheduler.delayMills", breakerboxConfiguration.getDelay());
+        ConfigurationManager.install(
+                new DynamicConfiguration(
+                    new URLConfigurationSource(breakerboxConfiguration.getUrls()),
+                    new TenacityPollingScheduler(
+                            breakerboxConfiguration.getInitialDelay(),
+                            breakerboxConfiguration.getDelay(),
+                            false)));
     }
 }
