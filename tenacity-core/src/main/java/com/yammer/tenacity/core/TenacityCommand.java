7c371df65973f3e0acd7cab63c162f150b00e6e1
cgray <cgray@yammer-inc.com>
2013-09-09 17:22:24 -0700

    Static helpers in TenacityCommand.

--- tenacity-core/src/main/java/com/yammer/tenacity/core/TenacityCommand.java
+++ tenacity-core/src/main/java/com/yammer/tenacity/core/TenacityCommand.java
@@ -7,24 +7,24 @@ import com.yammer.tenacity.core.properties.TenacityPropertyKey;
 public abstract class TenacityCommand<ReturnType> extends HystrixCommand<ReturnType> {
     protected TenacityCommand(TenacityPropertyKey tenacityPropertyKey) {
         super(HystrixCommand.Setter.withGroupKey(commandGroupKeyFrom("TENACITY"))
-                .andCommandKey(commandKeyFrom(tenacityPropertyKey.toString()))
-                .andThreadPoolKey(HystrixThreadPoolKey.Factory.asKey(tenacityPropertyKey.toString())));
+                .andCommandKey(commandKeyFrom(tenacityPropertyKey))
+                .andThreadPoolKey(threadpoolKeyFrom(tenacityPropertyKey)));
     }
 
     public static HystrixCommandGroupKey commandGroupKeyFrom(String key) {
         return HystrixCommandGroupKey.Factory.asKey(key);
     }
 
-    public static HystrixCommandKey commandKeyFrom(String key) {
-        return HystrixCommandKey.Factory.asKey(key);
+    public static HystrixCommandKey commandKeyFrom(TenacityPropertyKey key) {
+        return HystrixCommandKey.Factory.asKey(key.toString());
     }
 
-    public static HystrixThreadPoolKey threadpoolKeyFrom(String key) {
-        return HystrixThreadPoolKey.Factory.asKey(key);
+    public static HystrixThreadPoolKey threadpoolKeyFrom(TenacityPropertyKey key) {
+        return HystrixThreadPoolKey.Factory.asKey(key.toString());
     }
 
     public static HystrixCommandProperties getCommandProperties(TenacityPropertyKey key) {
-        return HystrixPropertiesFactory.getCommandProperties(commandKeyFrom(key.toString()), null);
+        return HystrixPropertiesFactory.getCommandProperties(commandKeyFrom(key), null);
     }
 
     public HystrixCommandProperties getCommandProperties() {
@@ -32,7 +32,7 @@ public abstract class TenacityCommand<ReturnType> extends HystrixCommand<ReturnT
     }
 
     public static HystrixThreadPoolProperties getThreadpoolProperties(TenacityPropertyKey key) {
-        return HystrixPropertiesFactory.getThreadPoolProperties(threadpoolKeyFrom(key.toString()), null);
+        return HystrixPropertiesFactory.getThreadPoolProperties(threadpoolKeyFrom(key), null);
     }
 
     public HystrixThreadPoolProperties getThreadpoolProperties() {
@@ -43,12 +43,24 @@ public abstract class TenacityCommand<ReturnType> extends HystrixCommand<ReturnT
         return HystrixCommandMetrics.getInstance(getCommandKey());
     }
 
+    public static HystrixCommandMetrics getCommandMetrics(TenacityPropertyKey key) {
+        return HystrixCommandMetrics.getInstance(commandKeyFrom(key));
+    }
+
     public HystrixThreadPoolMetrics getThreadpoolMetrics() {
         return HystrixThreadPoolMetrics.getInstance(getThreadPoolKey());
     }
 
+    public static HystrixThreadPoolMetrics getThreadpoolMetrics(TenacityPropertyKey key) {
+        return HystrixThreadPoolMetrics.getInstance(threadpoolKeyFrom(key));
+    }
+
     public HystrixCircuitBreaker getCircuitBreaker() {
         return HystrixCircuitBreaker.Factory.getInstance(getCommandKey());
+    }
+
+    public static HystrixCircuitBreaker getCircuitBreaker(TenacityPropertyKey key) {
+        return HystrixCircuitBreaker.Factory.getInstance(commandKeyFrom(key));
     }
 
     @Override
