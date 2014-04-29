d8d1927531d9c9e40c565f719d21be1ebdfb328f
cgray <cgray@yammer-inc.com>
2014-04-29 14:54:24 -0700

    Removing tenacity-clients dependency on metrics-2.2.0 and instead just leveraging @Timed

--- tenacity-client/src/main/java/com/yammer/tenacity/client/TenacityClient.java
+++ tenacity-client/src/main/java/com/yammer/tenacity/client/TenacityClient.java
@@ -1,11 +1,9 @@
 package com.yammer.tenacity.client;
 
+import com.codahale.metrics.annotation.Timed;
 import com.google.common.base.Optional;
 import com.google.common.collect.ImmutableList;
 import com.sun.jersey.api.client.Client;
-import com.yammer.metrics.Metrics;
-import com.yammer.metrics.core.Timer;
-import com.yammer.metrics.core.TimerContext;
 import com.yammer.tenacity.core.config.TenacityConfiguration;
 import com.yammer.tenacity.core.core.CircuitBreaker;
 import com.yammer.tenacity.core.properties.TenacityPropertyKey;
@@ -16,72 +14,57 @@ import javax.ws.rs.core.MediaType;
 import java.net.URI;
 
 public class TenacityClient {
-    private final Client client;
     public static final String TENACITY_PROPERTYKEYS_PATH = "/tenacity/propertykeys";
     public static final String TENACITY_CONFIGURATION_PATH = "/tenacity/configuration";
     public static final String TENACITY_CIRCUITBREAKERS_PATH = "/tenacity/circuitbreakers";
     private static final Logger LOGGER = LoggerFactory.getLogger(TenacityClient.class);
-    private static final Timer TIMER_GET_CONFIGURATION = Metrics.newTimer(TenacityClient.class, "get-configuration");
-    private static final Timer TIMER_GET_PROPERTYKEYS = Metrics.newTimer(TenacityClient.class, "get-propertykeys");
-    private static final Timer TIME_GET_CIRCUITBREAKERS = Metrics.newTimer(TenacityClient.class, "get-circuitbreakers");
+    protected final Client client;
 
     public TenacityClient(Client client) {
         this.client = client;
     }
 
+    @Timed
     public Optional<ImmutableList<String>> getTenacityPropertyKeys(URI root) {
-        final TimerContext timerContext = TIMER_GET_PROPERTYKEYS.time();
         try {
-            try {
-                return Optional.of(ImmutableList.copyOf(client.resource(root)
-                        .path(TENACITY_PROPERTYKEYS_PATH)
-                        .accept(MediaType.APPLICATION_JSON_TYPE)
-                        .get(String[].class)));
-            } catch (Exception err) {
-                LOGGER.warn("Unable to retrieve property keys for {}", root, err);
-            }
-            return Optional.absent();
-        } finally {
-            timerContext.stop();
+            return Optional.of(ImmutableList.copyOf(client.resource(root)
+                    .path(TENACITY_PROPERTYKEYS_PATH)
+                    .accept(MediaType.APPLICATION_JSON_TYPE)
+                    .get(String[].class)));
+        } catch (Exception err) {
+            LOGGER.warn("Unable to retrieve property keys for {}", root, err);
         }
+        return Optional.absent();
     }
 
+    @Timed
     public Optional<TenacityConfiguration> getTenacityConfiguration(URI root, TenacityPropertyKey key) {
-        final TimerContext timerContext = TIMER_GET_CONFIGURATION.time();
         try {
-            try {
-                return Optional.of(client
-                        .resource(root)
-                        .path(TENACITY_CONFIGURATION_PATH)
-                        .path(key.toString())
-                        .accept(MediaType.APPLICATION_JSON_TYPE)
-                        .get(TenacityConfiguration.class));
-            } catch (Exception err) {
-                LOGGER.warn("Unable to retrieve tenacity configuration for {} and key {}", root, key, err);
-            }
-            return Optional.absent();
-        } finally {
-            timerContext.stop();
+            return Optional.of(client
+                    .resource(root)
+                    .path(TENACITY_CONFIGURATION_PATH)
+                    .path(key.toString())
+                    .accept(MediaType.APPLICATION_JSON_TYPE)
+                    .get(TenacityConfiguration.class));
+        } catch (Exception err) {
+            LOGGER.warn("Unable to retrieve tenacity configuration for {} and key {}", root, key, err);
         }
+        return Optional.absent();
     }
 
 
+    @Timed
     public Optional<ImmutableList<CircuitBreaker>> getCircuitBreakers(URI root) {
-        final TimerContext timerContext = TIME_GET_CIRCUITBREAKERS.time();
         try {
-            try {
-                return Optional.of(ImmutableList.copyOf(client
-                        .resource(root)
-                        .path(TENACITY_CIRCUITBREAKERS_PATH)
-                        .accept(MediaType.APPLICATION_JSON_TYPE)
-                        .get(CircuitBreaker[].class)));
-            } catch (Exception err) {
-                LOGGER.warn("Unable to retrieve tenacity configuration for {} and key {}", root, err);
-            }
-            return Optional.absent();
-        } finally {
-            timerContext.stop();
+            return Optional.of(ImmutableList.copyOf(client
+                    .resource(root)
+                    .path(TENACITY_CIRCUITBREAKERS_PATH)
+                    .accept(MediaType.APPLICATION_JSON_TYPE)
+                    .get(CircuitBreaker[].class)));
+        } catch (Exception err) {
+            LOGGER.warn("Unable to retrieve tenacity configuration for {} and key {}", root, err);
         }
+        return Optional.absent();
     }
 
     @Override
