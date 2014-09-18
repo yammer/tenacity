186879ae24d1107c48b58dd1a722bf880d59ed65
Michał Rutkowski <mrutkowski@yammer-inc.com>
2014-09-18 15:43:39 +0100

    fixed spelling

--- tenacity-core/src/main/java/com/yammer/tenacity/core/bundle/TenacityBundleConfigurationFactory.java
+++ tenacity-core/src/main/java/com/yammer/tenacity/core/bundle/TenacityBundleConfigurationFactory.java
@@ -10,7 +10,7 @@ import java.util.Map;
 
 public interface TenacityBundleConfigurationFactory<T extends Configuration> {
 
-    Map<TenacityPropertyKey, TenacityConfiguration> getTenacityConfigurations(T applicatoinConfiguration);
+    Map<TenacityPropertyKey, TenacityConfiguration> getTenacityConfigurations(T applicationConfiguration);
 
     TenacityPropertyKeyFactory getTenacityPropertyKeyFactory(T applicationConfiguration);
 
