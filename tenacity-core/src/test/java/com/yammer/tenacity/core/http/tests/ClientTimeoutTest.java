b4d0dc27763485298949b27156d3f56b4823476d
Michał Rutkowski <mrutkowski@yammer-inc.com>
2015-03-10 19:57:01 +0000

    Updated the test for when dw bug removed.
    New Jersey client has a different architecture, so we don't need to test async/sync calls or different methods independently.
    - test that timeout actually occur when not enough set
    - test reconfiguration
    - test override
    
    (simpler tests now)

--- tenacity-core/src/test/java/com/yammer/tenacity/core/http/tests/ClientTimeoutTest.java
+++ tenacity-core/src/test/java/com/yammer/tenacity/core/http/tests/ClientTimeoutTest.java
@@ -15,7 +15,6 @@ import com.yammer.tenacity.testing.TenacityTestRule;
 import com.yammer.tenacity.tests.DependencyKey;
 import io.dropwizard.Application;
 import io.dropwizard.Configuration;
-import io.dropwizard.client.JerseyClientBuilder;
 import io.dropwizard.client.JerseyClientConfiguration;
 import io.dropwizard.jackson.Jackson;
 import io.dropwizard.setup.Bootstrap;
@@ -29,6 +28,7 @@ import org.junit.ClassRule;
 import org.junit.Ignore;
 import org.junit.Rule;
 import org.junit.Test;
+import org.junit.rules.ExpectedException;
 
 import javax.ws.rs.DELETE;
 import javax.ws.rs.DefaultValue;
@@ -42,15 +42,13 @@ import javax.ws.rs.QueryParam;
 import javax.ws.rs.client.Client;
 import javax.ws.rs.client.Entity;
 import javax.ws.rs.client.WebTarget;
-import javax.ws.rs.core.HttpHeaders;
-import javax.ws.rs.core.MediaType;
 import java.net.SocketTimeoutException;
 import java.net.URI;
-import java.util.Locale;
 import java.util.concurrent.ExecutionException;
 import java.util.concurrent.ExecutorService;
 import java.util.concurrent.Executors;
 
+import static org.hamcrest.CoreMatchers.any;
 import static org.junit.Assert.assertTrue;
 import static org.mockito.Mockito.mock;
 import static org.mockito.Mockito.spy;
@@ -67,38 +65,6 @@ public class ClientTimeoutTest {
         @POST
         public void post(@QueryParam("time")
                          @DefaultValue("100") long sleepTimeMs) throws InterruptedException {
-            doSleep(sleepTimeMs);
-        }
-
-        @HEAD
-        public void head(@QueryParam("time")
-                         @DefaultValue("100") long sleepTimeMs) throws InterruptedException {
-            doSleep(sleepTimeMs);
-        }
-
-        @GET
-        public String get(@QueryParam("time")
-                          @DefaultValue("100") long sleepTimeMs) throws InterruptedException {
-            doSleep(sleepTimeMs);
-            return "test";
-        }
-
-        @OPTIONS
-        public String options(@QueryParam("time")
-                              @DefaultValue("100") long sleepTimeMs) throws InterruptedException {
-            doSleep(sleepTimeMs);
-            return "test";
-        }
-
-        @PUT
-        public void put(@QueryParam("time")
-                        @DefaultValue("100") long sleepTimeMs) throws InterruptedException {
-            doSleep(sleepTimeMs);
-        }
-
-        @DELETE
-        public void delete(@QueryParam("time")
-                           @DefaultValue("100") long sleepTimeMs) throws InterruptedException {
             doSleep(sleepTimeMs);
         }
     }
@@ -120,6 +86,10 @@ public class ClientTimeoutTest {
 
     @Rule
     public final TenacityTestRule tenacityTestRule = new TenacityTestRule();
+    @Rule
+    public ExpectedException thrown = ExpectedException.none();
+
+
     @ClassRule
     public static DropwizardAppRule<Configuration> RULE;
     private static final BarrierTarget barrierTarget = new BarrierTarget();
@@ -155,25 +125,6 @@ public class ClientTimeoutTest {
                 .build("test'");
     }
 
-    private WebTarget postWithExpectedTimeout(Client client, Duration timeout) {
-        boolean exceptionThrown = false;
-        final WebTarget spyTarget = spy(client.target(uri));
-        try {
-            spyTarget
-                    .queryParam("time", Long.toString(timeout.toMilliseconds()))
-                    .request()
-                    .post(Entity.text(null));
-        } catch (Exception err) {
-            exceptionThrown = err.getCause() instanceof SocketTimeoutException;
-            synchronized (barrierTarget) {
-                barrierTarget.notifyAll();
-            }
-        }
-
-        assertTrue(exceptionThrown);
-        return spyTarget;
-    }
-
     private void registerTenacityProperties() {
         new TenacityPropertyRegister(
                 ImmutableMap.<TenacityPropertyKey, TenacityConfiguration>of(DependencyKey.CLIENT_TIMEOUT, tenacityConfiguration),
@@ -183,105 +134,75 @@ public class ClientTimeoutTest {
     }
 
     @Test
-    public void originalJerseyClientTimesOut() {
-        clientConfiguration.setTimeout(Duration.milliseconds(100));
-        postWithExpectedTimeout(buildClient(), Duration.seconds(2));
-    }
-
-    @Test
-    public void originalJerseyClientDoesntTimeOut() {
-        clientConfiguration.setTimeout(Duration.seconds(2));
-        final Client client = buildClient();
-
-        client.target(uri).queryParam("time", "100").request().post(null);
-    }
-
-    @Ignore("under investigation") // todo <michal>
-    @Test
-    public void jerseyClientTimeoutOverride() {
-        clientConfiguration.setTimeout(Duration.milliseconds(100));
-        final Client client = buildClient();
-
-        setReadTimeout(client, Duration.seconds(2));
-        client.target(uri).queryParam("time", "500").request().post(null);
-    }
-
-
-    @Ignore("under investigation") // todo <michal>
-    @Test
-    public void jerseyClientTimeoutOverrideToFail() {
-        clientConfiguration.setTimeout(Duration.seconds(2));
-        final Client client = buildClient();
-
-        setReadTimeout(client, Duration.milliseconds(100));
-        postWithExpectedTimeout(client, Duration.milliseconds(500));
-    }
-
-    @Ignore("under investigation") // todo <michal>
-    @Test
-    public void jerseyClientSupportsMultipleTimeoutChanges() {
-        clientConfiguration.setTimeout(Duration.milliseconds(100));
-
-        final Client client = buildClient();
-
-        setReadTimeout(client, Duration.milliseconds(1));
-        client.target(uri).queryParam("time", "500").request().post(null);
-
-        setReadTimeout(client, Duration.milliseconds(100));
-        postWithExpectedTimeout(client, Duration.milliseconds(500));
-
-        setReadTimeout(client, Duration.milliseconds(500));
-        client.target(uri).queryParam("time", "100"); // todo <michal> is this correct? it does nothing
-    }
-
-    @Ignore("under investigation") // todo <michal>
-    @Test
-    public void tenacityClientOverridesOriginalTimeout() {
+    public void tenacityClient_adds_padding_to_the_timeout() {
         clientConfiguration.setTimeout(Duration.seconds(1));
-        tenacityConfiguration.setExecutionIsolationThreadTimeoutInMillis(100);
-
+        tenacityConfiguration.setExecutionIsolationThreadTimeoutInMillis(480);
         registerTenacityProperties();
+        final Client client = tenacityClientBuilder.build(buildClient());
+        final WebTarget webTarget = client.target(uri);
 
-        final WebTarget spyTarget = postWithExpectedTimeout(tenacityClientBuilder
-                        .usingTimeoutPadding(Duration.milliseconds(43))
-                        .build(buildClient()),
-                Duration.milliseconds(500)
-        );
-
-        // todo <michal> this seems to whitebox, replace with behavioral?
-        //       verify(spyTarget, times(1)).setProperty(ClientConfig.PROPERTY_READ_TIMEOUT, 143);
+        postSettingTheTimeoutOnResource(webTarget, Duration.milliseconds(500));
     }
 
-    @Ignore("under investigation") // todo <michal>
     @Test
     public void tenacityClientWorksWithChangingPropertiesDynamically() throws InterruptedException, ExecutionException {
         clientConfiguration.setTimeout(Duration.seconds(1));
-
         tenacityConfiguration.setExecutionIsolationThreadTimeoutInMillis(100);
+        registerTenacityProperties();
+        final Client client = tenacityClientBuilder.build(buildClient());
+        final WebTarget webTarget = client.target(uri);
+
+
+        // with timeout too short
+        thrown.expectCause(any(SocketTimeoutException.class));
+        postSettingTheTimeoutOnResource(webTarget, Duration.milliseconds(500));
+
+        // after reconfiguring a longer timeout
+        tenacityConfiguration.setExecutionIsolationThreadTimeoutInMillis(520);
+        registerTenacityProperties();
+
+        postSettingTheTimeoutOnResource(webTarget, Duration.milliseconds(500));
+    }
+
+
+
+    @Test
+    public void tenacityDoesntRaceWithJerseyTimeout() {
+        clientConfiguration.setTimeout(Duration.milliseconds(1));
+        tenacityConfiguration.setExecutionIsolationThreadTimeoutInMillis(300);
         registerTenacityProperties();
 
         final Client client = tenacityClientBuilder.build(buildClient());
+        final WebTarget spyTarget = spy(client.target(uri));
+        final VoidCommand command = new VoidCommand(spyTarget, Duration.milliseconds(500));
 
-        WebTarget spyTarget = postWithExpectedTimeout(client, Duration.milliseconds(500));
-        // todo <michal> behavioral?  verify(spyTarget, times(1)).setProperty(ClientConfig.PROPERTY_READ_TIMEOUT, 150);
+        boolean timeoutFailure = false;
+        try {
+            command.execute();
+        } catch (HystrixRuntimeException err) {
+            timeoutFailure = err.getFailureType().equals(HystrixRuntimeException.FailureType.TIMEOUT);
+        }
 
-        tenacityConfiguration.setExecutionIsolationThreadTimeoutInMillis(500);
-        registerTenacityProperties();
+        assertTrue(timeoutFailure);
+        assertTrue(command.isResponseTimedOut());
+    }
 
-        spyTarget = spy(client.target("http://localhost:" + RULE.getLocalPort()));
-        spyTarget.path("/").queryParam("time", "100").request().post(null);
-        // todo <michal> behavioral?     verify(spyTarget, times(1)).setProperty(ClientConfig.PROPERTY_READ_TIMEOUT, 550);
-        final WebTarget spyAsyncTarget = spy(client.target("http://localhost:" + RULE.getLocalPort() + '/'));
-        spyAsyncTarget.path("/").queryParam("time", "100").request().async().post(null).get();
-        // todo <michal> behavioral?       verify(spyAsyncTarget, times(1)).setProperty(ClientConfig.PROPERTY_READ_TIMEOUT, 550);
+    @Test
+    public void tenacity_configuration_overrides_default_configuration() {
+        clientConfiguration.setTimeout(Duration.milliseconds(1));
+        final Client tenacityClient = tenacityClientBuilder.build(buildClient());
 
-        tenacityConfiguration.setExecutionIsolationThreadTimeoutInMillis(325);
-        registerTenacityProperties();
+        postSettingTheTimeoutOnResource(tenacityClient.target(uri), Duration.milliseconds(100));
 
-        spyTarget.path("/").queryParam("time", "100").request().post(null);
-        // todo <michal> behavioral? verify(spyTarget, times(1)).setProperty(ClientConfig.PROPERTY_READ_TIMEOUT, 375);
-        spyAsyncTarget.path("/").queryParam("time", "100").request().async().post(null).get();
-        // todo <michal> behavioral? verify(spyAsyncTarget, times(1)).setProperty(ClientConfig.PROPERTY_READ_TIMEOUT, 375);
+    }
+
+    @Test
+    public void regularClientTimesOut() {
+        clientConfiguration.setTimeout(Duration.milliseconds(1));
+        final Client regularClientWithNoTenacityOverride = buildClient();
+
+        thrown.expectCause(any(SocketTimeoutException.class));
+        postSettingTheTimeoutOnResource(regularClientWithNoTenacityOverride.target(uri), Duration.milliseconds(100));
     }
 
     private static class VoidCommand extends TenacityCommand<Void> {
@@ -296,138 +217,16 @@ public class ClientTimeoutTest {
 
         @Override
         protected Void run() throws Exception {
-            webTarget.queryParam("time", Long.toString(sleepDuration.toMilliseconds())).request().post(null);
+            postSettingTheTimeoutOnResource(webTarget, sleepDuration);
             return null;
         }
     }
 
-    @Ignore("under investigation") // todo <michal>
-    @Test
-    public void tenacityDoesntRaceWithJerseyTimeout() {
-        clientConfiguration.setTimeout(Duration.milliseconds(1));
-        tenacityConfiguration.setExecutionIsolationThreadTimeoutInMillis(300);
-        registerTenacityProperties();
-
-        final Client client = tenacityClientBuilder.build(buildClient());
-        final WebTarget spyTarget = spy(client.target(uri));
-        final VoidCommand command = new VoidCommand(spyTarget, Duration.milliseconds(500));
-
-        boolean timeoutFailure = false;
-        long startTime = System.currentTimeMillis();
-        try {
-            command.execute();
-        } catch (HystrixRuntimeException err) {
-            timeoutFailure = err.getFailureType().equals(HystrixRuntimeException.FailureType.TIMEOUT);
-        }
-        long endTime = System.currentTimeMillis();
-        System.err.println("===============|" + (endTime - startTime));
-
-        assertTrue(timeoutFailure);
-        assertTrue(command.isResponseTimedOut());
-
-        // todo <michal> verify(spyTarget, times(1)).setProperty(ClientConfig.PROPERTY_READ_TIMEOUT, 350);
-    }
-
-    @Ignore("under investigation") // todo <michal>
-    @Test
-    public void adjustTimeoutOnWebTarget() {
-        final WebTarget target = buildClient()
-                .target(uri);
-
-        target.getConfiguration()
-                .getProperties()
-                .put(ClientProperties.READ_TIMEOUT, 200);
-
-        target.queryParam("time", "150")
+    private static void postSettingTheTimeoutOnResource(WebTarget webTarget, Duration timeout) {
+        webTarget
+                .queryParam("time", Long.toString(timeout.toMilliseconds()))
                 .request()
-                .post(null);
-    }
-
-    @Ignore("under investigation") // todo <michal>
-    @Test
-    public void tenacityWebTargetAdjustTimeoutForPost() {
-        clientConfiguration.setTimeout(Duration.milliseconds(1));
-        tenacityConfiguration.setExecutionIsolationThreadTimeoutInMillis(75); //will be 125ms after the 50ms buffer from the TenacityClientBuilder for 75+25=125ms
-        registerTenacityProperties();
-        final Client tenacityClient = tenacityClientBuilder.build(buildClient());
-
-        final WebTarget spyTarget = spy(tenacityClient.target(uri));
-        spyTarget.request().post(null);
-
-        // todo <michal> behavioral? verify(spyTarget, times(1)).setProperty(ClientConfig.PROPERTY_READ_TIMEOUT, 125);
-    }
-
-    @Ignore("under investigation") // todo <michal>
-    @Test // todo <michal> is this really needed, seems hard to maintain
-    public void adjustTimeoutWhenUsingDifferentMethods() {
-        clientConfiguration.setTimeout(Duration.milliseconds(1));
-        tenacityConfiguration.setExecutionIsolationThreadTimeoutInMillis(150);
-        registerTenacityProperties();
-        final Client tenacityClient = tenacityClientBuilder.build(buildClient());
-        final WebTarget spyTarget = spy(tenacityClient.target(uri));
-
-        int times = 0;
-
-        spyTarget.request().accept(MediaType.TEXT_PLAIN_TYPE).post(null);
-        // todo <michal> behavioral? verify(spyTarget, times(++times)).setProperty(ClientConfig.PROPERTY_READ_TIMEOUT, 200);
-
-        spyTarget.request().acceptLanguage(Locale.ENGLISH).post(null);
-        // todo <michal> behavioral? verify(spyTarget, times(++times)).setProperty(ClientConfig.PROPERTY_READ_TIMEOUT, 200);
-
-        spyTarget.request().header(HttpHeaders.AUTHORIZATION, "Something").post(null);
-        // todo <michal> behavioral? verify(spyTarget, times(++times)).setProperty(ClientConfig.PROPERTY_READ_TIMEOUT, 200);
-
-        spyTarget.request().head();
-        // todo <michal> behavioral? verify(spyTarget.request(), times(++times)).setProperty(ClientConfig.PROPERTY_READ_TIMEOUT, 200);
-
-        spyTarget.request().get(String.class);
-        // todo <michal> behavioral? verify(spyTarget.request(), times(++times)).setProperty(ClientConfig.PROPERTY_READ_TIMEOUT, 200);
-
-        spyTarget.request().put(null);
-        // todo <michal> behavioral? verify(spyTarget.request(), times(++times)).setProperty(ClientConfig.PROPERTY_READ_TIMEOUT, 200);
-
-        spyTarget.request().post(null);
-        // todo <michal> behavioral? verify(spyTarget.request(), times(++times)).setProperty(ClientConfig.PROPERTY_READ_TIMEOUT, 200);
-
-        spyTarget.request().options(String.class);
-        // todo <michal> behavioral? verify(spyTarget.request(), times(++times)).setProperty(ClientConfig.PROPERTY_READ_TIMEOUT, 200);
-
-        spyTarget.request().delete();
-        // todo <michal> behavioral? verify(spyTarget.request(), times(++times)).setProperty(ClientConfig.PROPERTY_READ_TIMEOUT, 200);
-
-        spyTarget.request().put(Entity.text("test"));
-        // todo <michal> behavioral? verify(spyTarget.request(), times(++times)).setProperty(ClientConfig.PROPERTY_READ_TIMEOUT, 200);
-
-        spyTarget.request().method("POST");
-        // todo <michal> behavioral?  verify(spyTarget.request(), times(++times)).setProperty(ClientConfig.PROPERTY_READ_TIMEOUT, 200);
-
-        spyTarget.request()
-                .header(HttpHeaders.AUTHORIZATION, "Something")
-                .acceptLanguage(Locale.ENGLISH)
                 .post(Entity.text(null));
-        // todo <michal> behavioral?  verify(spyTarget, times(++times)).setProperty(ClientConfig.PROPERTY_READ_TIMEOUT, 200);
-    }
-
-
-    @Test
-    public void noTenacityConfigurationSetShouldUseDefault() {
-        // this establishes the socket timeout on the http client (connectionTimeout is 100 and unmodified and connectionRequestTimeout is 500)
-        clientConfiguration.setTimeout(Duration.milliseconds(1));
-        // this casues the property to be udpated, but so far does not seem to propagate in any way to the http client
-        final Client tenacityClient = tenacityClientBuilder.build(buildClient());
-// todo <michal> probably remove this - instead add test that shows that we can generate timeout
-//        final WebTarget spyTarget = spy(tenacityClient.target(uri));
-//        spyTarget.request().post(null);
-        tenacityClient.target(uri).request().post(null);
-
-        // this row can be deleted - ultimately we don't care how it is done, but we want the timeout to be updated so that this test passes (we might want to add a timing out test)
-        // todo <michal> behavioral?  verify(spyTarget, times(1)).setProperty(ClientConfig.PROPERTY_READ_TIMEOUT, 1050); //Tenacity default + 50ms
-    }
-
-
-
-    private static void setReadTimeout(Client client, Duration duration) {
-        client.property(ClientProperties.READ_TIMEOUT, (int) duration.toMilliseconds());
     }
 
 }
