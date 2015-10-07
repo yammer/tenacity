f228327184ddcae1d3079e6de17f5f06d3b831b0
cgray <cgray@yammer-inc.com>
2015-10-07 11:50:30 -0700

    Adding TenacityTestRule to all tests

--- tenacity-core/src/test/java/com/yammer/tenacity/tests/TenacityConfigurationResourceTest.java
+++ tenacity-core/src/test/java/com/yammer/tenacity/tests/TenacityConfigurationResourceTest.java
@@ -1,22 +1,25 @@
 package com.yammer.tenacity.tests;
 
-import io.dropwizard.testing.junit.ResourceTestRule;
 import com.yammer.tenacity.core.config.TenacityConfiguration;
 import com.yammer.tenacity.core.properties.TenacityPropertyKeyFactory;
 import com.yammer.tenacity.core.resources.TenacityConfigurationResource;
+import com.yammer.tenacity.testing.TenacityTestRule;
+import io.dropwizard.testing.junit.ResourceTestRule;
 import org.junit.Before;
 import org.junit.Rule;
 import org.junit.Test;
+
 import static org.fest.assertions.api.Assertions.assertThat;
 import static org.mockito.Matchers.anyString;
-import static org.mockito.Mockito.mock;
-import static org.mockito.Mockito.when;
-import static org.mockito.Mockito.reset;
+import static org.mockito.Mockito.*;
 
 public class TenacityConfigurationResourceTest {
 
     public static final String TENACITY_CONFIGURATION_URI = "/tenacity/configuration";
     private TenacityPropertyKeyFactory mock = mock(TenacityPropertyKeyFactory.class);
+
+    @Rule
+    public final TenacityTestRule tenacityTestRule = new TenacityTestRule();
 
     @Rule
     public final ResourceTestRule resources = ResourceTestRule.builder()
--- tenacity-core/src/test/java/com/yammer/tenacity/tests/TenacityPropertyKeyResourceTest.java
+++ tenacity-core/src/test/java/com/yammer/tenacity/tests/TenacityPropertyKeyResourceTest.java
@@ -5,11 +5,14 @@ import com.google.common.collect.Lists;
 import com.sun.jersey.api.client.GenericType;
 import com.yammer.tenacity.core.properties.TenacityPropertyKey;
 import com.yammer.tenacity.core.resources.TenacityPropertyKeysResource;
+import com.yammer.tenacity.testing.TenacityTestRule;
+import io.dropwizard.testing.junit.ResourceTestRule;
 import org.junit.Rule;
 import org.junit.Test;
-import io.dropwizard.testing.junit.ResourceTestRule;
+
 import java.util.ArrayList;
 import java.util.List;
+
 import static org.fest.assertions.api.Assertions.assertThat;
 
 public class TenacityPropertyKeyResourceTest {
@@ -18,12 +21,16 @@ public class TenacityPropertyKeyResourceTest {
     private final ImmutableList<TenacityPropertyKey> keys = ImmutableList.<TenacityPropertyKey>of(DependencyKey.EXAMPLE, DependencyKey.SLEEP);
 
     @Rule
+    public final TenacityTestRule tenacityTestRule = new TenacityTestRule();
+
+    @Rule
     public final ResourceTestRule resources = ResourceTestRule.builder()
             .addResource(new TenacityPropertyKeysResource(keys)).build();
 
     @Test
     public void testGetKeys() throws Exception {
-        final Iterable<? extends TenacityPropertyKey> iterable = resources.client().resource(PROPERTY_KEY_URI).get(new GenericType<ArrayList<DependencyKey>>() { });
+        final Iterable<? extends TenacityPropertyKey> iterable = resources.client().resource(PROPERTY_KEY_URI).get(new GenericType<ArrayList<DependencyKey>>() {
+        });
         final List<TenacityPropertyKey> properties = Lists.newArrayList(iterable);
         for (TenacityPropertyKey key : properties) {
             assertThat(properties).contains(key);
